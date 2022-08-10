1ebb240d7241a66a5fdaa8aaba4dd611706eaa37
Sahrul-Maulana <sahrulprjkt@gmail.com>
2022-08-10 13:27:11 +0700

    .

---
+++ kernel.sh
@@ -0,0 +1,3 @@
+git config --global user.email sahrulprjkt@gmail.com
+git config --global user.name Sahrul-Maulana
+git clone https://Sahrul-Maulana:$GH_TOKEN@github.com/Sahrul-Maulana/android_kernel_xiaomi_ginkgo -b lineage-19.1
